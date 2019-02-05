31db06b9bad6567ae8f89ad38909fb24d87e0e50
shreyhan <shreyhan@Shreyhans-MacBook-Air.local>
2019-02-05 22:57:58 +0530

    kisck ban

--- commands/ban.js
+++ commands/ban.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, msg, args) => {
 	if (!breason) return msg.channel.send(`Please Specify Why you Want To Ban ${bUser}`);
 	if(!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.send("You Cannot Ban People!")
 	if (msg.author = kUser) return;
-if (!bUser.manageable) console.log(`${msg.author} tried to ban ${bUser} but failed`);
+if (!bUser.manageable) return console.log(`${msg.author} tried to ban ${bUser} but failed`);
 	// if(bUser.hasPermission("BAN_MEMBERS")) return msg.channel.send("You Cannot Ban That Person")
 
 
