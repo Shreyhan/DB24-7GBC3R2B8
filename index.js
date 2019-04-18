45874f7db00114f4e53c47adaa97d8d89541715e
shreyhan <shreyhan@Shreyhans-MacBook-Air.local>
2019-04-18 20:46:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ bot.on("message", async(msg) => {
 		if(msg.author.id === `253796217820151808`) {
 			msg.channel.send("`Restarting...`").then(function(m) {
 				m.edit("Restarted!");
-			}
+			})
     	msg.channel.send(`Logged in as ${bot.user}!`)
 		} else {
 			msg.channel.send(`you are not my owner...`)
